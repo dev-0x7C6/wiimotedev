f2c1619e8634c4aaeedbd934357c47cb958753c5
Bartłomiej Burdukiewicz <dev.strikeu@gmail.com>
2010-01-15 16:50:54 +0000

    uncommented #define.

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-//#define __daemon
+#define __daemon
 
 #ifdef __daemon
     #include <sys/types.h>
