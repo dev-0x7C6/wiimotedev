d3ff00975794b73020d4cf0d6bd341a1a9d61b85
Bartłomiej Burdukiewicz <dev.strikeu@gmail.com>
2010-09-26 15:30:14 +0000

    Some integration with uinput.

--- src/uinput.cpp
+++ src/uinput.cpp
@@ -41,13 +41,26 @@
 #include <unistd.h>
 #include <string.h>
 
-#include "wiimotedev/manager.h"
+#include "adaptors/adaptors.h"
 #include "syslog/syslog.h"
 
+
+#include "consts.h"
+
 #include <QCoreApplication>
+#include <QFile>
+#include <QMap>
+#include <QSettings>
+#include <QStringList>
+#include <QTextStream>
 #include <QScopedPointer>
 
+const QString scancodeFile("/etc/wiimotedev/scancode.conf");
+
 QScopedPointer <QCoreApplication> application;
+QMap < QString, quint64> devicebuttons;
+QMap < QString, quint16> scancodes;
+
 
 bool additional_debug = false;
 
@@ -73,7 +86,7 @@ int main(int argc, char *argv[])
     qDebug("  --help\t\tprint help page");
     qDebug("  --no-daemon\t\tdo not run in background");
     qDebug("  --no-quiet\t\tdo not block stdout messages");
-    qDebug();
+    qDebug("");
     exit(EXIT_SUCCESS);
   }
 
@@ -114,6 +127,78 @@ int main(int argc, char *argv[])
   if (additional_debug)
     systemlog::debug("additional debug mode switch-on");
 
+  QSettings settings(scancodeFile, QSettings::IniFormat);
+  settings.beginGroup("scancode");
+
+  QStringList list = settings.allKeys();
+  for (register qint32 i = 0; i < settings.allKeys().count(); ++i)
+      scancodes[QString(settings.allKeys().at(i)).toLower().remove(QChar(' '))] = settings.value(settings.allKeys().at(i), 0).toInt();
+
+  settings.endGroup();
+
+  devicebuttons.insert("wiimote.1", WIIMOTE_BTN_1);
+  devicebuttons.insert("wiimote.2", WIIMOTE_BTN_2);
+  devicebuttons.insert("wiimote.a", WIIMOTE_BTN_A);
+  devicebuttons.insert("wiimote.b", WIIMOTE_BTN_B);
+  devicebuttons.insert("wiimote.plus", WIIMOTE_BTN_PLUS);
+  devicebuttons.insert("wiimote.minus", WIIMOTE_BTN_MINUS);
+  devicebuttons.insert("wiimote.home", WIIMOTE_BTN_HOME);
+  devicebuttons.insert("wiimote.up", WIIMOTE_BTN_UP);
+  devicebuttons.insert("wiimote.down", WIIMOTE_BTN_DOWN);
+  devicebuttons.insert("wiimote.left", WIIMOTE_BTN_LEFT);
+  devicebuttons.insert("wiimote.right", WIIMOTE_BTN_RIGHT);
+  devicebuttons.insert("wiimote.shift.up", WIIMOTE_BTN_SHIFT_UP);
+  devicebuttons.insert("wiimote.shift.down", WIIMOTE_BTN_SHIFT_DOWN);
+  devicebuttons.insert("wiimote.shift.left", WIIMOTE_BTN_SHIFT_LEFT);
+  devicebuttons.insert("wiimote.shift.right", WIIMOTE_BTN_SHIFT_RIGHT);
+  devicebuttons.insert("wiimote.shift.forward", WIIMOTE_BTN_SHIFT_FORWARD);
+  devicebuttons.insert("wiimote.shift.backward", WIIMOTE_BTN_SHIFT_BACKWARD);
+  devicebuttons.insert("wiimote.tilt.front", WIIMOTE_BTN_TILT_FRONT);
+  devicebuttons.insert("wiimote.tilt.back", WIIMOTE_BTN_TILT_BACK);
+  devicebuttons.insert("wiimote.tilt.left", WIIMOTE_BTN_TILT_LEFT);
+  devicebuttons.insert("wiimote.tilt.right", WIIMOTE_BTN_TILT_RIGHT);
+  devicebuttons.insert("nunchuk.z", NUNCHUK_BTN_Z);
+  devicebuttons.insert("nunchuk.c", NUNCHUK_BTN_C);
+  devicebuttons.insert("nunchuk.stick.up", NUNCHUK_BTN_STICK_UP);
+  devicebuttons.insert("nunchuk.stick.down", NUNCHUK_BTN_STICK_DOWN);
+  devicebuttons.insert("nunchuk.stick.left", NUNCHUK_BTN_STICK_LEFT);
+  devicebuttons.insert("nunchuk.stick.right", NUNCHUK_BTN_STICK_RIGHT);
+  devicebuttons.insert("nunchuk.shift.up", NUNCHUK_BTN_SHIFT_UP);
+  devicebuttons.insert("nunchuk.shift.down", NUNCHUK_BTN_SHIFT_DOWN);
+  devicebuttons.insert("nunchuk.shift.left", NUNCHUK_BTN_SHIFT_LEFT);
+  devicebuttons.insert("nunchuk.shift.right", NUNCHUK_BTN_SHIFT_RIGHT);
+  devicebuttons.insert("nunchuk.shift.forward", NUNCHUK_BTN_SHIFT_FORWARD);
+  devicebuttons.insert("nunchuk.shift.backward", NUNCHUK_BTN_SHIFT_BACKWARD);
+  devicebuttons.insert("nunchuk.tilt.front", NUNCHUK_BTN_TILT_FRONT);
+  devicebuttons.insert("nunchuk.tilt.back", NUNCHUK_BTN_TILT_BACK);
+  devicebuttons.insert("nunchuk.tilt.left", NUNCHUK_BTN_TILT_LEFT);
+  devicebuttons.insert("nunchuk.tilt.right", NUNCHUK_BTN_TILT_RIGHT);
+  devicebuttons.insert("classic.x", CLASSIC_BTN_X);
+  devicebuttons.insert("classic.y", CLASSIC_BTN_Y);
+  devicebuttons.insert("classic.a", CLASSIC_BTN_A);
+  devicebuttons.insert("classic.b", CLASSIC_BTN_B);
+  devicebuttons.insert("classic.l", CLASSIC_BTN_L);
+  devicebuttons.insert("classic.r", CLASSIC_BTN_R);
+  devicebuttons.insert("classic.zl", CLASSIC_BTN_ZL);
+  devicebuttons.insert("classic.zr", CLASSIC_BTN_ZR);
+  devicebuttons.insert("classic.minus", CLASSIC_BTN_MINUS);
+  devicebuttons.insert("classic.plus", CLASSIC_BTN_PLUS);
+  devicebuttons.insert("classic.home", CLASSIC_BTN_HOME);
+  devicebuttons.insert("classic.up", CLASSIC_BTN_UP);
+  devicebuttons.insert("classic.down", CLASSIC_BTN_DOWN);
+  devicebuttons.insert("classic.left", CLASSIC_BTN_LEFT);
+  devicebuttons.insert("classic.right", CLASSIC_BTN_RIGHT);
+  devicebuttons.insert("classic.lstick.up", CLASSIC_BTN_LSTICK_UP);
+  devicebuttons.insert("classic.lstick.down", CLASSIC_BTN_LSTICK_DOWN);
+  devicebuttons.insert("classic.lstick.left", CLASSIC_BTN_LSTICK_LEFT);
+  devicebuttons.insert("classic.lstick.right", CLASSIC_BTN_LSTICK_RIGHT);
+  devicebuttons.insert("classic.rstick.up", CLASSIC_BTN_RSTICK_UP);
+  devicebuttons.insert("classic.rstick.down", CLASSIC_BTN_RSTICK_DOWN);
+  devicebuttons.insert("classic.rstick.left", CLASSIC_BTN_RSTICK_LEFT);
+  devicebuttons.insert("classic.rstick.right", CLASSIC_BTN_RSTICK_RIGHT);
+  devicebuttons.insert("wiimote.shift.shake", WIIMOTE_BTN_SHIFT_SHAKE);
+  devicebuttons.insert("nunchuk.shift.shake", NUNCHUK_BTN_SHIFT_SHAKE);
+
   qRegisterMetaType< QList< irpoint> >("QList< irpoint>");
   qRegisterMetaType< QList< accdata> >("QList< accdata>");
   qRegisterMetaType< QList< stickdata> >("QList< stickdata>");
@@ -122,13 +207,9 @@ int main(int argc, char *argv[])
   qRegisterMetaType< accdata>("accdata");
   qRegisterMetaType< stickdata>("stickdata");
 
-  ConnectionManager *manager_thread = new ConnectionManager();
-  manager_thread->start(QThread::HighPriority);
-  application.take()->exec();
-  manager_thread->terminateRequest();
-  manager_thread->wait();
+  ProfileManager profileManager;
 
-  delete manager_thread;
+  application.take()->exec();
 
   systemlog::information("system service closed");
   systemlog::close();
---
+++ src/uinput/manager.h
@@ -0,0 +1,296 @@
+/**********************************************************************************
+ * Wiimotedev uinput, virtual devices system service                              *
+ * Copyright (C) 2010  Bartlomiej Burdukiewicz                                    *
+ * Contact: dev.strikeu@gmail.com                                                 *
+ *                                                                                *
+ * This library is free software; you can redistribute it and/or                  *
+ * modify it under the terms of the GNU Lesser General Public                     *
+ * License as published by the Free Software Foundation; either                   *
+ * version 2.1 of the License, or (at your option) any later version.             *
+ *                                                                                *
+ * This library is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              *
+ * Lesser General Public License for more details.                                *
+ *                                                                                *
+ * You should have received a copy of the GNU Lesser General Public               *
+ * License along with this library; if not, write to the Free Software            *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA *
+ **********************************************************************************/
+
+#ifndef PROFILEMANAGER_H
+#define PROFILEMANAGER_H
+
+#include <QtCore/QObject>
+#include <QtCore/QMetaType>
+#include <QRegExp>
+#include <Qt/QtDBus>
+
+#include <cwiid.h>
+#include "consts.h"
+
+#include "customjobs.h"
+#include "profilemanager.h"
+#include "uinputservice.h"
+#include "adaptors.h"
+
+/*
+class VirtualDeviceManager :public QObject
+{
+    Q_OBJECT
+private:
+// Standard devices -----------------------------------------------
+    UInputKeyboard *keyboard;
+    UInputMouse *relativeMouse;
+    UInputMouse *absoluteMouse;
+
+// Gamepads devices -----------------------------------------------
+    QMap < quint32, *UInputClassicGamepad> virtualClassicGamepads;
+    QMap < quint32, *UInputWiimoteGamepad> virtualWiimoteGamepads;
+
+public:
+    VirtualDeviceManager(QObject *parent = 0) :QObject(parent){
+        keyboard = new UInputKeyboard;
+        relativeMouse = new UInputMouse();
+        absoluteMouse = new UInputMouse();
+
+        keyboard->uinput_open();
+        relativeMouse->uinput_open();
+        absoluteMouse->uinput_open(QRect(-512, -384, 1024, 768), true);
+    };
+
+    ~VirtualDeviceManager(){
+        disconnect(this, 0, 0, 0);
+
+        keyboard->uinput_close();
+        relativeMouse->uinput_close();
+        absoluteMouse->uinput_close();
+
+        QMapIterator < quint32, *UInputClassicGamepad> classicIterator(virtualClassicGamepads);
+        QMapIterator < quint32, *UInputWiimoteGamepad> wiimoteIterator(virtualWiimoteGamepads);
+
+        while (classicIterator.hasNext()){
+            static_cast< UInputClassicGamepad*>( classicIterator.value())->uinput_close();
+            delete classicIterator.value();
+        }
+
+        while (wiimoteIterator.hasNext()){
+            static_cast< UInputWiimoteGamepad*>( wiimoteIterator.value())->uinput_close();
+            delete wiimoteIterator.value();
+        }
+
+        delete keyboard;
+        delete relativeMouse;
+        delete absoluteMouse;
+    }
+
+    void initVirtualClassicGamepad(QList < quint32> &devices);
+    void freeVirtualClassicGamepad(QList < quint32> &devices);
+    void freeAllVirtualClassicGamepad();
+
+    void initVirtualWiimoteGamepad(QList < quint32> &devices);
+    void freeVirtualWiimoteGamepad(QList < quint32> &devices);
+    void freeAllVirtualWiimoteGamepad();
+
+};
+*/
+
+class itemTemplate
+{
+private:
+    bool actived;
+
+public:
+    inline void setActivedValue(bool status) { actived = status; };
+    inline bool getActivedValue() { return actived; };
+};
+
+class eventItem: public itemTemplate
+{  
+public:
+    QMap < qint32, quint64> event;
+    QList < quint16> scancodes;
+};
+
+class execItem: public itemTemplate
+{
+public:
+    QMap < qint32, quint64> event;
+    QString exec;
+};
+
+class actionItem: public itemTemplate
+{
+public:
+    QMap < qint32, quint64> event;
+    QString action;
+};
+
+
+
+enum {
+    mouseEmulationModeNone = 0,
+    mouseEmulationModeAbs,
+    mouseEmulationModeAcc
+};
+
+enum {
+    mouseEmulationAlghoritm1point = 1,
+    mouseEmulationAlghoritm2points,
+    mouseEmulationAlghoritm3points,
+    mouseEmulationAlghoritm4points
+};
+
+struct mouseEmulationStruct {
+    quint8 mode;
+    quint8 alghoritm;
+};
+
+
+struct InfraredConfigurationStruct {
+    quint8 mode;
+    quint8 alghoritm;
+};
+
+namespace profiles
+{
+    namespace infrared
+    {
+        const QString section("infrared/");
+        const QString wiimoteid("wiimoteid");
+        const QString mode("mode");
+        const QString alghoritm("alghoritm");
+        const QString sensx("sensitivity.x");
+        const QString sensy("sensitivity.y");
+        const QString freezonex("freezone.x");
+        const QString freezoney("freezone.y");
+        const QString timeout("timeout");
+        const QString latency("latency");
+        const QString range("range");
+    }
+}
+
+class ProfileManager : public QObject
+{
+    Q_OBJECT
+public:
+    ProfileManager(QObject *parent = 0);
+   ~ProfileManager();
+
+private:          
+
+/* Infrared section ---------------------------------------------- */
+
+    qint32 x;
+    qint32 y;
+
+    quint32 irWiimoteId;
+    quint8 irMode;
+    quint8 irAlghoritm;
+    double irXSensitivity;
+    double irYSensitivity;
+    quint32 irXFreeZone;
+    quint32 irYFreeZone;
+    quint32 irTimeout;
+    quint32 irLatency;
+
+    qint32 moveX;
+    qint32 moveY;
+    QTimer infraredTimer;
+    QTimer infraredTimeout;
+    QRect irRange;
+
+    bool timeout;
+
+/* DBus section -------------------------------------------------- */
+
+    DBusProfileManagerAdaptorWrapper *dbusProfileManager;
+    DBusServiceAdaptorWrapper *dbusService;
+    DBusCustomJobsAdaptorWrapper *dbusCustomJobs;
+
+    DBusDeviceEventsInterface *dbusDeviceEventsIface;
+
+/* Uinput section ------------------------------------------------ */
+
+    UInputClassicGamepad *virtualClassicGamepad;
+    UInputWiimoteGamepad *virtualWiimoteGamepad;
+
+    UInputEvent *virtualEvent;
+    UInputMouse *virtualAbsoluteMouse;
+
+/* Execute section ----------------------------------------------- */
+
+    QList < eventItem*> events;
+    QList < eventItem*> eventsOnce;
+    QList < execItem*> execs;
+    QList < execItem*> execsReleased;
+    QList < actionItem*> actions;
+    QList < actionItem*> actionsRelease;
+
+/* General variables --------------------------------------------- */
+
+    QString profileName;
+    QMap < quint32, quint64> buttons;
+
+    qint32 lastX;
+    qint32 lastY;
+    QPoint cursor;
+
+private:
+    QMap < qint32, quint64> extractDeviceEvent(QString str);
+    QList < quint16> extractScancodes(QStringList list);
+
+public slots:
+    inline bool isWiimotedevServiceAvailable(){ return dbusDeviceEventsIface->isValid(); }
+    inline QString currentProfile(){ return profileName; }
+
+    bool loadProfile(QString file);
+    bool unloadProfile();
+
+private slots:
+    inline void slotDBusClassicControllerButtons(quint32 id, quint64 buttons) {
+        virtualClassicGamepad->classicButtons(id, buttons);
+    }
+
+    inline void slotDBusWiimoteAcc(quint32 id, struct accdata acc) {
+        virtualWiimoteGamepad->wiimoteAcc(id, acc);
+    }
+
+    inline void slotDBusWiimoteButtons(quint32 id, quint64 buttons) {
+        virtualWiimoteGamepad->wiimoteButtons(id, buttons);
+    }
+
+    inline void slotDBusNunchukAcc(quint32 id, struct accdata acc) {
+        virtualWiimoteGamepad->nunchukAcc(id, acc);
+    }
+
+    inline void slotDBusNunchukButtons(quint32 id, quint64 buttons) {
+        virtualWiimoteGamepad->nunchukButtons(id, buttons);
+    }
+
+    inline void slotDBusNunchukStick(quint32 id, struct stickdata stick) {
+        virtualWiimoteGamepad->nunchukStick(id, stick.x, 0xFF - stick.y);
+    }
+
+    inline void slotDBusClassicControllerLStick(quint32 id, struct stickdata stick) {
+        virtualClassicGamepad->classicLStick(id, stick.x, 0x3F - stick.y);
+    }
+
+    inline void slotDBusClassicControllerRStick(quint32 id, struct stickdata stick) {
+        virtualClassicGamepad->classicRStick(id, stick.x, 0x1F - stick.y);
+    }
+
+
+    bool dbusWiimoteGetRumbleStatus(quint32 id){};
+    bool dbusWiimoteSetLedStatus(quint32 id, quint8 status){};
+    bool dbusWiimoteSetRumbleStatus(quint32 id, bool status){};
+    quint8 dbusWiimoteGetLedStatus(quint32 id){};
+
+    void dbusWiimoteGeneralButtons(quint32 id, quint64 value);
+    void dbusWiimoteInfrared(quint32 id, QList< struct irpoint> points);
+
+    void infraredTimeoutSection(){ timeout = true; };
+    void infraredAccSection();
+};
+
+#endif // PROFILEMANAGER_H
