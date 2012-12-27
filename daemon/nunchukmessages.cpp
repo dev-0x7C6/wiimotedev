54d2e55a0bb561b8f33705fff76a656101dd77f0
Bartłomiej Burdukiewicz <dev.strikeu@gmail.com>
2012-12-28 00:30:26 +0100

    Code cleanups in nunchukmessages.cpp

--- daemon/nunchukmessages.cpp
+++ daemon/nunchukmessages.cpp
@@ -28,10 +28,8 @@ void WiimoteMessageThread::cwiid_process_nunchuk_done() {
 }
 
 void WiimoteMessageThread::cwiid_process_nunchuk_clear() {
-  cstate[ix_nunchuk_device] = 0x00;
-  lstate[ix_nunchuk_device] = 0x00;
-  stick[ix_nunchuk_stick].x = 0x00;
-  stick[ix_nunchuk_stick].y = 0x00;
+  cstate[ix_nunchuk_device] = lstate[ix_nunchuk_device] =
+  stick[ix_nunchuk_stick].x = stick[ix_nunchuk_stick].y = 0x00;
   setDeviceAvailable(ix_nunchuk_device, false);
 }
 
@@ -42,6 +40,7 @@ void WiimoteMessageThread::cwiid_process_nunchuk_buttons(quint8 cwiid_buttons) {
 }
 
 void WiimoteMessageThread::cwiid_process_nunchuk_stick(quint8 cwiid_stick[2]) {
+  cstate[ix_nunchuk_device] &= NUNCHUK_STICK_NOTMASK;
   if (stick[ix_nunchuk_stick].x != cwiid_stick[ix_x_axis] ||
       stick[ix_nunchuk_stick].y != cwiid_stick[ix_y_axis]) {
     stick[ix_nunchuk_stick].x = cwiid_stick[ix_x_axis];
@@ -49,7 +48,6 @@ void WiimoteMessageThread::cwiid_process_nunchuk_stick(quint8 cwiid_stick[2]) {
     emit dbusNunchukStick(m_id, stick[ix_nunchuk_stick]);
   }
 
-  cstate[ix_nunchuk_device] &= NUNCHUK_STICK_NOTMASK;
   if (stick[ix_nunchuk_stick].x > nunchukStickMaxX) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_STICK_RIGHT; else
   if (stick[ix_nunchuk_stick].x < nunchukStickMinX) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_STICK_LEFT;
   if (stick[ix_nunchuk_stick].y > nunchukStickMaxY) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_STICK_UP; else
@@ -57,14 +55,14 @@ void WiimoteMessageThread::cwiid_process_nunchuk_stick(quint8 cwiid_stick[2]) {
 }
 
 void WiimoteMessageThread::cwiid_process_nunchuk_acc(quint8 cwiid_acc[3]) {
-  calcAccelerometerValues(cwiid_acc, calibration[ix_nunchuk_device], acc[ix_nunchuk_device]);
-  emit dbusNunchukAcc(m_id, acc[ix_nunchuk_device]);
-
   cstate[ix_nunchuk_device] &= NUNCHUK_TILT_NOTMASK;
+  calcAccelerometerValues(cwiid_acc, calibration[ix_nunchuk_device], acc[ix_nunchuk_device]);
+
   if (acc[ix_wiimote_device].pitch < -45.0) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_TILT_FRONT; else
   if (acc[ix_wiimote_device].pitch >  45.0) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_TILT_BACK;
   if (acc[ix_wiimote_device].roll >  45.0) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_TILT_RIGHT; else
   if (acc[ix_wiimote_device].roll < -45.0) cstate[ix_nunchuk_device] |= NUNCHUK_BTN_TILT_LEFT;
+  emit dbusNunchukAcc(m_id, acc[ix_nunchuk_device]);
 }
 
 void WiimoteMessageThread::cwiid_process_nunchuk_status(cwiid_ext_type type) {
@@ -83,7 +81,6 @@ void WiimoteMessageThread::cwiid_process_nunchuk_status(cwiid_ext_type type) {
     }
     break;
   default:
-    break;
   }
 }
 
