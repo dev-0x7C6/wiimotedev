e0664af4bf7d77f789a2a7339ccd46cb3afc3749
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2016-10-16 21:04:54 +0200

    Renamed many project files.

--- Version.cmake
+++
@@ -1,9 +0,0 @@
-# Distributed under the terms of the GNU Lesser General Public License, v2.1 or later
-# Copyright (C) 2008 Bartłomiej Burdukiewicz
-# Contact: bartlomiej.burdukiewicz@gmail.com
-
-set(WIIMOTEDEV_SETTINGS_FILE "/etc/wiimotedev/wiimotedev.conf")
-set(WIIMOTEDEV_CONNECTIONS_FILE "/etc/wiimotedev/connections.conf")
-set(WIIMOTEDEV_VERSION_MAJOR 2)
-set(WIIMOTEDEV_VERSION_MINOR 9)
-set(WIIMOTEDEV_VERSION_PATCH 0)
