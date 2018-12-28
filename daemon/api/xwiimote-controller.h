36c51013f5c430cf4d4f4e60a9bf7b111f820b04
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2018-12-28 01:35:18 +0100

    Removed logger and call_on_destroy class and moved them to externals.

--- daemon/api/xwiimote-controller.h
+++ daemon/api/xwiimote-controller.h
@@ -8,112 +8,12 @@
 #include <queue>
 #include <string>
 
+#include <externals/common/logger/logger.hpp>
+#include <externals/common/std/raii/raii-tail-call.hpp>
+
 #include "interfaces/iwiimote-api.h"
 
-enum class error_class {
-	critical,
-	error,
-	warning,
-	information,
-	notice,
-	hint,
-	debug,
-};
-
-template <error_class filter_above = error_class::information, std::ostream &out = std::cout, std::ostream &err = std::cerr>
-class logger {
-public:
-	template <error_class id = error_class::information, typename... args>
-	constexpr static void log(args &&... values) {
-		if constexpr (filter_above >= id) {
-			switch (id) {
-				case error_class::critical:
-				case error_class::error:
-				case error_class::warning:
-					print_error(std::forward<args>(values)...);
-					break;
-				case error_class::information:
-				case error_class::notice:
-				case error_class::hint:
-				case error_class::debug:
-					print_standard(std::forward<args>(values)...);
-					break;
-			}
-		}
-	}
-
-	template <typename... args>
-	constexpr static void critical(args &&... values) {
-		log<error_class::critical>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void error(args &&... values) {
-		log<error_class::error>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void warning(args &&... values) {
-		log<error_class::warning>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void information(args &&... values) {
-		log<error_class::information>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void notice(args &&... values) {
-		log<error_class::notice>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void hint(args &&... values) {
-		log<error_class::hint>(std::forward<args>(values)...);
-	}
-
-	template <typename... args>
-	constexpr static void debug(args &&... values) {
-		log<error_class::debug>(std::forward<args>(values)...);
-	}
-
-protected:
-	template <typename... args>
-	constexpr static void print_error(args &&... values) {
-		((out << values), ...) << std::endl;
-	}
-
-	template <typename... args>
-	constexpr static void print_standard(args &&... values) {
-		((err << values), ...) << std::endl;
-	}
-};
-
-class call_on_destroy {
-public:
-	call_on_destroy(std::function<void()> &&callable)
-			: m_callable(std::move(callable)) {}
-
-	call_on_destroy(call_on_destroy &&rhs) noexcept = default;
-	call_on_destroy(call_on_destroy const &) = delete;
-
-	call_on_destroy &operator=(call_on_destroy &&) noexcept = default;
-	call_on_destroy &operator=(call_on_destroy const &) = delete;
-	call_on_destroy &operator=(std::function<void()> &&callable) noexcept {
-		m_callable = callable;
-		return *this;
-	}
-
-	~call_on_destroy() {
-		if (m_callable)
-			m_callable();
-	}
-
-private:
-	std::function<void()> m_callable;
-};
-
-using destruction_queue = std::queue<call_on_destroy>;
+using destruction_queue = std::queue<raii_tail_call>;
 
 struct xwii_iface;
 struct xwii_event;
