ff1d4933222c9c06041093bd213705263f6dffc6
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2022-08-30 18:16:31 +0200

    daemon: fixed memory leak from xwii_monitor_poll
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- daemon/api/xwiimote-monitor.cpp
+++ daemon/api/xwiimote-monitor.cpp
@@ -17,11 +17,18 @@ bool XWiimoteMonitor::isValid() const {
 	return m_monitor != nullptr;
 }
 
+namespace cpp {
+auto xwii_monitor_poll(xwii_monitor *monitor) -> std::string {
+	std::string ret;
+	auto cstr = ::xwii_monitor_poll(monitor);
+	if (cstr != nullptr) {
+		ret = std::string(cstr);
+		free(cstr);
+	}
+	return ret;
+}
+}
+
 std::string XWiimoteMonitor::dequeue() {
-	auto cstring = xwii_monitor_poll(m_monitor);
-
-	if (cstring == nullptr)
-		return {};
-
-	return cstring;
+	return cpp::xwii_monitor_poll(m_monitor);
 }
