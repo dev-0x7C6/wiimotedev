487527474a7c7b5da3ee20a30ba689ee709fdd03
Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>
2022-09-15 23:52:01 +0200

    daemon: drop unused id generator classes.
    Signed-off-by: Bartłomiej Burdukiewicz <bartlomiej.burdukiewicz@gmail.com>

--- daemon/controllers/wiimote-id-manager.h
+++ daemon/controllers/wiimote-id-manager.h
@@ -7,39 +7,6 @@
 #include <externals/common/raii/raii-value-assigment.hpp>
 #include <externals/common/types.hpp>
 
-using raii_atomic_assigment = raii_value_assigment<std::atomic_bool &, true, false>;
-
-class unique_id_locker {
-public:
-	unique_id_locker(raii_atomic_assigment &&lock, u32 id)
-			: m_lock(std::move(lock))
-			, m_id(id) {}
-
-	constexpr auto id() const noexcept { return m_id; }
-
-private:
-	raii_atomic_assigment m_lock;
-	const u32 m_id{};
-};
-
-class UniqueIdReservation {
-public:
-	unique_id_locker take() {
-		for (std::size_t i = 0; i < m_reserved.size(); ++i) {
-			auto &place = m_reserved[i];
-			if (place)
-				return unique_id_locker(raii_atomic_assigment(place), i);
-		}
-
-		// TODO: id managment to be improved
-		const auto idx = m_reserved.size() - 1;
-		return unique_id_locker(raii_atomic_assigment(m_reserved[idx]), idx);
-	}
-
-private:
-	std::array<std::atomic_bool, 128> m_reserved{};
-};
-
 namespace dae::controller {
 
 class WiimoteIdManager final : public interface::IIdManager {
